6d39f09cdcac228cae328e96824b2a556f7077ba
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-06-18 21:29:10 +0700

    Add Delegate Realtime Example

---
+++ DelegatesRealTimeExampleCsharp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DelegatesRealTimeExampleCsharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WorkPerformedHandler del1 = new WorkPerformedHandler(Worker_WorkPerformed);
+        WorkCompletedHandler del2 = new WorkCompletedHandler(Worker_WorkCompleted);
+
+        Worker worker = new Worker();
+        worker.DoWork(5, "Generating Report", del1, del2);
+    }
+
+    //Delegate Signature must match with the method signature
+    static void Worker_WorkPerformed(int hours, string workType)
+    {
+        Console.WriteLine($"{hours} Hours compelted for {workType}");
+    }
+    static void Worker_WorkCompleted(string workType)
+    {
+        Console.WriteLine($"{workType} work compelted");
+    }
+}
